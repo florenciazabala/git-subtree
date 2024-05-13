c6ab7c5cd1ea82481556448148b4b10f3f4a3cf9
Florencia Agustina Zabala <fzabala@prismamp.com>
2024-05-13 10:16:20 -0300

    feat: Initial commit

---
+++ src/main/java/com/florenciazabala/gitsubtree/GitSubtreeApplication.java
@@ -0,0 +1,13 @@
+package com.florenciazabala.gitsubtree;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitSubtreeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitSubtreeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/florenciazabala/gitsubtree/GitSubtreeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.florenciazabala.gitsubtree;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitSubtreeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
